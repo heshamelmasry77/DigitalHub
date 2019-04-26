99e49ee939e7978fad9f80ae71f5f19881f61c37
hesham <heshamelmasry1992@gmail.com>
2019-04-26 14:40:51 +0300

    caching responses

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   lintOnSave: true,
   pwa: {
-    name: "My App",
-    themeColor: "#4DBA87",
-    msTileColor: "#000000",
-    appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black",
-
     // configure the workbox plugin
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
